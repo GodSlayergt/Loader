06acb13295fd9df4500764697aca0899e5c8a999
gt037@gmail.com <gautiwar6@corp.psengineering.io>
2021-09-29 12:59:05 +0530

    corrected useScriptLoader

--- customhook/useScriptLoader.js
+++ customhook/useScriptLoader.js
@@ -1,41 +1,35 @@
-import { useState} from 'react';
-const useScriptLoader = (url,id,selector,deferloading)=>{
-    const [loaded, setLoaded] = useState(false);
+import { useState } from "react";
+const useScriptLoader = (url, id, selector, deferloading) => {
+  const [loaded, setLoaded] = useState(false);
 
-    const appendScript = (parent)=>{
-        const existingScript = document.getElementById(id);
-        if(existingScript){
-            setLoaded(true)
-            return
-        }
-        const script = document.createElement('script');
-        script.src =  url;
-        script.id = id;
-        script.defer= deferloading
-        parent.appendChild(script);
-        script.onload =()=>setLoaded(true)
-
+  const appendScript = (parent) => {
+    const existingScript = document.getElementById(id);
+    if (existingScript) {
+      setLoaded(true);
+      return;
     }
-    const loader = () => {
-        if(!process.browser) {
-             setLoaded(true)
-             return null;
-        }
+    const script = document.createElement("script");
+    script.src = url;
+    script.id = id;
+    script.defer = deferloading;
+    parent.appendChild(script);
+    script.onload = () => setLoaded(true);
+  };
+  const loader = () => {
+    if (!process.browser) {
+      setLoaded(true);
+      return null;
+    }
 
-        const parent =  document.getElementById(selector);
-        if(parent){
-            appendScript(parent)
-            
-        }
-        else{
-            appendScript(document.body,url,id)
-        }
-       
-        
-      };
+    const parent = document.getElementById(selector);
+    if (parent) {
+      appendScript(parent);
+    } else {
+      appendScript(document.body);
+    }
+  };
 
-      return {loaded,loader}
+  return { loaded, loader };
+};
 
-}
- 
 export default useScriptLoader;
